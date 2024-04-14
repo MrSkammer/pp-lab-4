f759c62b749e1a090fe64feea1eebda25903e1e8
Kamil <mrkamkam2053@gmail.com>
2024-04-14 11:56:50 +0200

    3

--- Company.java
+++ Company.java
@@ -11,10 +11,23 @@ public class Company {
         employees[3] = new Employee("Bob Budowniczy", 4000);
         employees[4] = new Worker("Dawid Satyra", 3500, "Builder");
 
-        System.out.println("Data of Emplyee number 3:");
+        int countOfEmployees = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                countOfEmployees++;
+            }
+        }
+        
+        System.out.println(countOfEmployees);
+
+        Manager manager = (Manager) employees [0];
+        manager.setNumberOfSubordinates((countOfEmployees));
+
+        System.out.println("Data of Employee number 3:");
         System.out.println(employees[3]);
 
         employees[3].setSalary(5000);
+        employees[0].setSalary(7500);
 
         System.out.println("Data of all employees:");
         for (Employee employee : employees) {
