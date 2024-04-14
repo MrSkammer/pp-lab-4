7e9e4daa8d566e7aa41ac59f8c3b73f8a3b53497
Kamil <mrkamkam2053@gmail.com>
2024-04-14 12:13:07 +0200

    4

--- Company.java
+++ Company.java
@@ -3,13 +3,20 @@ import employees.*;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Anna Kowalska", 3000, 0);
         employees[1] = new Worker("Marek Daniel", 2000, "Dev");
         employees[2] = new Employee("Alicja Brown", 2500);
         employees[3] = new Employee("Bob Budowniczy", 4000);
         employees[4] = new Worker("Dawid Satyra", 3500, "Builder");
+        employees[5] = new Manager("Albert Koniuszko", 6000, 0);
+        employees[6] = new Worker("Agnieszka Mała", 3000, "Babysitter");
+
+        for (Employee employee : employees) {
+            double currentSalary = employee.getSalary();
+            employee.setSalary(currentSalary + 500);
+        }
 
         int countOfEmployees = 0;
         for (Employee employee : employees) {
@@ -17,11 +24,14 @@ public class Company {
                 countOfEmployees++;
             }
         }
-        
-        System.out.println(countOfEmployees);
 
-        Manager manager = (Manager) employees [0];
-        manager.setNumberOfSubordinates((countOfEmployees));
+        for (Employee employee : employees) {
+            if (employee instanceof Manager) {
+                Manager manager = (Manager) employee;
+                manager.setNumberOfSubordinates((countOfEmployees));
+                manager.setSalary(7500);
+            }
+        }
 
         System.out.println("Data of Employee number 3:");
         System.out.println(employees[3]);
